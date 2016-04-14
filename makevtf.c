31e433ae44d47d27bfc1100834b3d1c16dfdf8f5
sigurdas <sigurd.aurvaag.sorum@gmail.com>
2016-04-14 18:26:05 +0200

    tok vek nodeid

--- makevtf.c
+++ makevtf.c
@@ -7,49 +7,116 @@
 int main(void){
 FILE *fpGNU =fopen("/home/sig/datamet/GNUres.dat","r");
 FILE *fpVTF =fopen("/home/sig/datamet/VTFres.vtf","w");
-int  inp, ref, counter=1,i;
+int  inp, ref, pos, counter=1, i, refmarker, startpos;
 double tid, p_value, i_value, d_value, algout, error, setservo;
 
 fscanf(fpGNU, "%lf %lf %lf %d %d %lf %lf %lf %lf",&tid,&error,&algout,&inp, &ref, &p_value, &i_value, &d_value, &setservo);
+
+startpos=inp;
+refmarker=(startpos-ref)*27/100;
+
 fprintf(fpVTF,"*VTF-1.00\n\n");
-//prøv 2D først
+
+//Nodes for boat
 fprintf(fpVTF,"*NODES 1\n");
-fprintf(fpVTF,"0. 0. 0.\n20. 0. 0.\n10. 20. 0.\n\n");//geometri, nodes
-fprintf(fpVTF,"*NODES 2\n");//geometri ref
-fprintf(fpVTF,"-50 %d 0.\n50 %d 0.\n\n", ref, ref);//geometri
+fprintf(fpVTF,"-7 5 5\n");
+fprintf(fpVTF,"7 5 5\n");
+fprintf(fpVTF,"5 5 -5\n");
+fprintf(fpVTF,"-5 5 -5\n");
+fprintf(fpVTF,"-9 25 5\n");
+fprintf(fpVTF,"9 25 5\n");
+fprintf(fpVTF,"-9 25 -5\n");
+fprintf(fpVTF,"9 25 -5\n");
+fprintf(fpVTF,"0 65 5\n");
+fprintf(fpVTF,"0 45 -5 \n\n");
 
-fprintf(fpVTF,"*ELEMENTS 1\n%%NODES #1\n%%TRIANGLES\n1 2 3\n\n");
-fprintf(fpVTF,"*ELEMENTS 2\n%%NODES #2 \n%%BEAMS\n1 2\n\n");
-fprintf(fpVTF,"*GLVIEWGEOMETRY 1\n%%ELEMENTS\n1,2\n\n");
 
-//for å lese fra fil
-while (1){
-  if( feof(fpGNU) ) {
-    break;
-  }
+//Nodes for waterplane
+fprintf(fpVTF,"*NODES 2\n");
+fprintf(fpVTF,"-25 0 0\n");
+fprintf(fpVTF,"25 0 0\n");
+fprintf(fpVTF,"-25 110 0\n");
+fprintf(fpVTF,"25 110 0\n\n");
 
-fprintf(fpVTF,"*RESULTS %d\n",counter);
-fprintf(fpVTF,"%%DIMENSION 2\n");
-fprintf(fpVTF,"%%PER_NODE #1\n");
-fprintf(fpVTF,"0, %d\n",inp);
-fprintf(fpVTF,"0, %d\n",inp);
-fprintf(fpVTF,"0, %d\n\n",inp);
-counter= counter+1;
-fscanf(fpGNU, "%lf %lf %lf %d %d %lf %lf %lf %lf",&tid,&error,&algout,&inp, &ref, &p_value, &i_value, &d_value, &setservo);
-}//eo while
+//Nodes for referance marker
+fprintf(fpVTF,"*NODES 3\n");
+
+fprintf(fpVTF,"-25 %d 1\n", refmarker);
+fprintf(fpVTF,"-25 %d -1\n", refmarker);
+fprintf(fpVTF,"25 %d 1\n", refmarker);
+fprintf(fpVTF,"25 %d -1\n\n", refmarker);
+
+//Element 1 Boat
+fprintf(fpVTF,"*ELEMENTS 1\n");
+fprintf(fpVTF,"%%NODES #1\n");
+fprintf(fpVTF,"%%HEXAHEDRONS\n");
+fprintf(fpVTF,"4 3 8 7 1 2 6 5\n\n");
+fprintf(fpVTF,"%%PENTAHEDRONS\n");
+fprintf(fpVTF,"8 10 7 6 9 5\n\n");
+
+
+//Element 2 Waterplane
+fprintf(fpVTF,"*ELEMENTS 2\n");
+fprintf(fpVTF,"%%NODES #2\n");
+fprintf(fpVTF,"%%QUADS\n");
+fprintf(fpVTF,"1 2 4 3\n\n");
+
+//Element 3 reference marker
+fprintf(fpVTF,"*ELEMENTS 3\n");
+fprintf(fpVTF,"%%NODES #3\n");
+fprintf(fpVTF,"%%QUADS\n");
+fprintf(fpVTF,"2 4 3 1\n\n");
+
+//View geometry
+fprintf(fpVTF,"*GLVIEWGEOMETRY 1\n");
+fprintf(fpVTF,"%%ELEMENTS\n");
+fprintf(fpVTF,"1,2,3\n\n");
+
+
+  while (1){
+
+          if( feof(fpGNU) ) {
+              break;
+            }//eo if
+
+        //Calculate position of boat in cm
+        pos=(startpos-inp)*27/100;
+
+            //Print Results
+            fprintf(fpVTF,"*RESULTS %d\n",counter);
+            fprintf(fpVTF,"%%DIMENSION 3\n");
+            fprintf(fpVTF,"%%PER_NODE #1\n");
+            fprintf(fpVTF,"0, %d, 0\n",pos);
+            fprintf(fpVTF,"0, %d, 0\n",pos);
+            fprintf(fpVTF,"0, %d, 0\n",pos);
+            fprintf(fpVTF,"0, %d, 0\n",pos);
+            fprintf(fpVTF,"0, %d, 0\n",pos);
+            fprintf(fpVTF,"0, %d, 0\n",pos);
+            fprintf(fpVTF,"0, %d, 0\n",pos);
+            fprintf(fpVTF,"0, %d, 0\n",pos);
+            fprintf(fpVTF,"0, %d, 0\n",pos);
+            fprintf(fpVTF,"0, %d, 0\n\n",pos);
+
+
+        counter= counter+1;
+
+        fscanf(fpGNU, "%lf %lf %lf %d %d %lf %lf %lf %lf",&tid,&error,&algout,&inp, &ref, &p_value, &i_value, &d_value, &setservo);
+
+      }//eo while
 
 
 fprintf(fpVTF,"*GLVIEWVECTOR 1\n");
-fprintf(fpVTF,"%%NAME \"POSITION\" \n");
+fprintf(fpVTF,"%%NAME \"POSITION\" \n\n");
 
+      //Print steps
+      for(i=1; i<counter;i++){
+          fprintf(fpVTF,"%%STEP %d\n %d\n",i,i);
+        }//eo if
 
-for(i=1; i<counter;i++){
-fprintf(fpVTF,"%%STEP %d\n %d\n",i,i);
-}
-
-fclose (fpGNU);
+//Close filepointers
+fclose(fpGNU);
 fclose(fpVTF);
+
 return 0;
+
 }
-// ide: definere(og måle) tanken, ha den som boks, ha vannnivå oppi,+ hoveddim
-//på båt. gjøre om error til feil i cm, og ha et plan som viser refpkt.
